3d2c27342404a8c3df8f7cac332e6e0a70b7d21c
Nikita <vns95@bk.ru>
2024-12-08 12:34:29 +0300

    feat: add case-converter feature

--- libs/feature-case-converter/src/lib/feature-case-converter/feature-case-converter.component.ts
+++ libs/feature-case-converter/src/lib/feature-case-converter/feature-case-converter.component.ts
@@ -1,21 +1,47 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FeatureAddTaskComponent } from '@todo-list/feature-add-task';
-import { TodoItemComponent } from '@todo-list/todo-item';
-import { TuiInputModule } from '@taiga-ui/legacy';
+import { TuiInputModule, TuiTextareaModule } from '@taiga-ui/legacy';
 import { TuiButton } from '@taiga-ui/core';
+import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'lib-feature-case-converter',
   standalone: true,
   imports: [
     CommonModule,
-    FeatureAddTaskComponent,
-    TodoItemComponent,
     TuiInputModule,
     TuiButton,
+    TuiTextareaModule,
+    FormsModule,
   ],
   templateUrl: './feature-case-converter.component.html',
   styleUrl: './feature-case-converter.component.less',
 })
-export class FeatureCaseConverterComponent {}
+export class FeatureCaseConverterComponent {
+
+  private caseTextSubject = new BehaviorSubject<string>('');
+  public caseText: string = '';
+
+  constructor() {
+    this.caseTextSubject.subscribe(text => {
+      this.caseText = text;
+    });
+  }
+
+  public onUpperCase() {
+    this.caseTextSubject.next(this.caseText.toUpperCase());
+  }
+
+  public onLowerCase() {
+    this.caseTextSubject.next(this.caseText.toLowerCase());
+  }
+
+  public onTextChanged(text: string) {
+    this.caseTextSubject.next(text);
+  }
+
+  public onCapitalize() {
+    this.caseTextSubject.next(this.caseText.toLowerCase().replace(/\b\w/g, c => c.toUpperCase()));
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,9 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { FeatureCaseConverterComponent } from '@todo-list/feature-case-converter';
 import { TodosListComponent } from '@todo-list/todos-list';
 
-// export const appRoutes: Route[] = [
-//   { path: 'home', component: HomePageComponent },
-// ];
-
 
 export const appRoutes: Route[] = [
-  { path: '', component: HomePageComponent }, // корень открывает HomePageComponent
-  { path: 'home', component: TodosListComponent },
+  { path: '',  redirectTo: '/todo-list', pathMatch: 'full' },
+  { path: 'todo-list', component: TodosListComponent },
   { path: 'case-converter', component: FeatureCaseConverterComponent },
 ];
